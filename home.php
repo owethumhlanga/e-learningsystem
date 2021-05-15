a3b85c9587d59e4cae64165f6e0814c4f36443a9
owethu.mhlanga <owethu.mhlanga10@gmail.com>
2021-05-15 03:26:02 +0200

    ready fot Github

---
+++ home.php
@@ -0,0 +1,46 @@
+<title>Student Dashbord</title>
+
+<div class="col-lg-12">  
+<h4>Mandate</h4>
+    <p> Our University was formed in 2004 following the merger of three technikons. Our merger aimed at redressing
+        the inequalities of the past by providing access to all our communities and an academic home for all our
+        students.</p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Vision</h4> 
+  <p>At TUT, we embrace engaged scholarship whereby learning, teaching, research and engagement
+	is integrated with our everyday realities.
+	Our University is committed towards breaking down the ivory towers of academia by finding authentic
+	and enduring solutions to our communities’ most pressing problems.
+	We empower our graduates by future-proofing them to successfully negotiate the rapidly changing
+	world of work and make a tangible movement towards civic renewal.</p>
+  <h4>Mission</h4>
+  <p>Our mission directs us towards solving pressing societal problems and ensuring that our graduates
+	are productive and active citizens.
+	Our suite of programmes and qualifications, research and innovation, as well as engagement with
+	stakeholders are aimed at making a positive impact on our communities.
+	We are a committed staff who works tirelessly towards providing a quality service and conducive
+	teaching, learning and living spaces.</h4> 
+
+
+   <div  class="col-lg-6">
+  <h4>Our values</h4> 
+    <ul>
+      <li>Intergrity </li>
+      <li>Care</li>
+      <li>Diversity, Inclusion, Equity</li>
+      <li>Excellence</li>
+      <li>Accoutability</li>  
+    </ul> 
+
+  <h4>Our Pillars and Goals</h4> 	
+  <ol>
+    <li>Future-ready graduates who make a positive societal impact.</li>  
+    <li>Impactful research, innovation, engagement and technology
+        transfer to foster growth, development and sustainability.</li> 
+    <li>Service and operational excellence through resource optimisation</li> 
+  </ol>
+
+
+
+
